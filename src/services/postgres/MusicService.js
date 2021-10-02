ccd6ae89e780fb1d08a69299ede3f44182a15c64
Regy Saputra <regysaputra27@gmail.com>
2021-10-03 01:33:54 +0700

    rev1

--- src/services/postgres/MusicService.js
+++ src/services/postgres/MusicService.js
@@ -41,7 +41,7 @@ class MusicService{
         };
         const result = await this._pool.query(query);
      
-        if (!result.rows.length) {
+        if (!result.rowCount) {
           throw new NotFoundError('Lagu tidak ditemukan');
         }
      
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,21 +1,27 @@
-const mapDBToModel = ({ 
-    id,
-    title,
-    year,
-    performer,
-    genre,
-    duration,
-    inserted_at,
-    updated_at
-}) => ({
-    id,
-    title,
-    year,
-    performer,
-    genre,
-    duration,
+// const mapDBToModel = ({ 
+//     id,
+//     title,
+//     year,
+//     performer,
+//     genre,
+//     duration,
+//     inserted_at,
+//     updated_at
+// }) => ({
+//     id,
+//     title,
+//     year,
+//     performer,
+//     genre,
+//     duration,
+//     insertedAt: inserted_at,
+//     updatedAt: updated_at
+// });
+
+const mapDBToModel = ({ inserted_at, updated_at, ...args }) => ({
+    ...args,
     insertedAt: inserted_at,
-    updatedAt: updated_at
+    updatedAt: updated_at,
 });
 
 module.exports = { mapDBToModel };
